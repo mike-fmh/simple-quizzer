c64c126cc4ef5715568287326ace669c0d1be3bd
Michael Miele-Herndon <mikemh@uri.edu>
2023-02-24 14:41:54 -0500

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='flashquiz',
-    version='0.0a1',
+    version='0.0rc1',
     description='[TEST VERSION] Quiz yourself on your flashcards',
     author='mike-fmh',
     author_email='mikemh@uri.edu',
