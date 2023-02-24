f7298e56e47530ed551eb69e85dc49be8f706562
Michael Miele-Herndon <mikemh@uri.edu>
2023-02-24 10:30:37 -0500

    define startign behavior

---
+++ flashquiz/flashcards/deck.py
@@ -0,0 +1,6 @@
+from card import Card
+
+
+class Deck:
+    def __init__(self, cards: [Card]):
+        self.cards = cards
---
+++ flashquiz/parse/csv.py
@@ -0,0 +1,9 @@
+import csv
+import os
+from ..flashcards.deck import Deck
+
+
+# For parsing csv documents into question | answer format
+
+def from_csv(filepath: str) -> Deck:
+    pass
