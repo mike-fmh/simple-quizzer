468b0d59beca657be1d4c9ef9345551c6c905a88
Michael Miele-Herndon <mikemh@uri.edu>
2023-02-26 10:09:25 -0500

    clock

--- flashquiz/gui/gui.py
+++ flashquiz/gui/gui.py
@@ -7,8 +7,11 @@ class GUI:
         self.window_name = opts.title
         self.dimensions = (opts.w, opts.h)
 
+    def reset_clock(self):
+        self.clock = pygame.time.Clock()
+
     def init_screen(self):
         pygame.init()
-        self.clock = pygame.time.Clock()
+        self.reset_clock()
         self.screen = pygame.display.set_mode(self.dimensions)
         pygame.display.set_caption(self.window_name)
