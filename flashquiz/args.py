1bde5a46d77ce88b35097c4bae856c4058f0e82b
Michael Miele-Herndon <mikemh@uri.edu>
2023-02-26 10:13:48 -0500

    more info

--- flashquiz/args.py
+++ flashquiz/args.py
@@ -6,18 +6,22 @@ def handle_args():
     """Create all arguments, and return the parsed given arguments"""
     args = ArgumentParser()
     args.add_argument("--file",
+                      metavar="INPUT_FILE",
                       help="Path to the file containing questions and answers for your flashcards")
-    args.add_argument("--title",
-                      type=str,
-                      help="Window title",
-                      default="FlashQuiz")
     args.add_argument("--h",
                       type=int,
+                      metavar="WIN_HEIGHT",
                       help="Window height",
                       default=Screen.height)
     args.add_argument("--w",
                       type=int,
+                      metavar="WIN_WIDTH",
                       help="Window width",
                       default=Screen.width)
+    args.add_argument("--title",
+                      type=str,
+                      metavar="WIN_TITLE",
+                      help="Window title",
+                      default="FlashQuiz")
 
     return args.parse_args()
