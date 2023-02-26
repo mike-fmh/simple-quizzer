20de5f50bb40161fa0d3e57db117953a96f0440d
Michael Miele-Herndon <mikemh@uri.edu>
2023-02-26 10:19:50 -0500

    remove test

--- flashquiz/quizzer.py
+++ flashquiz/quizzer.py
@@ -1,9 +1,20 @@
-import pygame
-from .flashcards.parser import from_csv
-from .args import handle_args
-from .gui import GUI
+from flashquiz.flashcards.deck import Deck
+from flashquiz.args import handle_args
+from flashquiz.gui import GUI
 
 
 def main():
     args = handle_args()
     GUI(args).init_screen()
+
+
+if __name__ == '__main__':
+    d = Deck()
+
+    d.add_card("hello", "world")
+    d.print()
+    print("\n")
+
+    main()
+    while True:
+        pass
