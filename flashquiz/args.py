592434f836e02d434dc2c9c2bf28a6201c42e406
Michael Miele-Herndon <mikemh@uri.edu>
2023-03-02 17:51:06 -0500

    fix relative paths, include misc files in package install

--- flashquiz/args.py
+++ flashquiz/args.py
@@ -1,21 +1,22 @@
 from argparse import ArgumentParser
 from .gui.constants import Screen
+from os import path
 
 
 def handle_args():
     """Create all arguments, and return the parsed given arguments"""
     args = ArgumentParser()
+    pkg_dir = path.dirname(path.abspath(__file__))
     args.add_argument("--fps",
                       metavar="FPS",
                       type=int,
                       help="Frames per second to run at",
                       default=30
                       ),
-
     args.add_argument("--file",
                       metavar="INPUT_FILE",
                       help="Path to the file containing questions and answers for your flashcards",
-                      default="default.csv")
+                      default=path.join(pkg_dir, "default.csv"))
     args.add_argument("--font",
                       metavar="FONT_NAME",
                       type=str,
@@ -24,11 +25,11 @@ def handle_args():
     args.add_argument("--cards_front",
                       metavar="IMG_PATH",
                       help="Path to the .jpg to use for all flashcard front backgrounds (500x250 pixels)",
-                      default="assets/card_front.jpg")
+                      default=path.join(pkg_dir, "assets/card_front.jpg"))
     args.add_argument("--cards_back",
                       metavar="IMG_PATH",
                       help="Path to the .jpg to use for all flashcard back backgrounds (500x250 pixels)",
-                      default="assets/card_back.jpg")
+                      default=path.join(pkg_dir, "assets/card_back.jpg"))
     args.add_argument("--h",
                       type=int,
                       metavar="WIN_HEIGHT",
