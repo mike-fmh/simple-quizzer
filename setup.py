4d7964493b8279d112a2fc110efd78814b321d57
Michael Miele-Herndon <mikemh@uri.edu>
2023-03-02 17:17:18 -0500

    make quizzer a console script

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='flashquiz',
-    version="0.0b1",
-    description='[TEST VERSION] Quiz yourself on your flashcards',
+    version="0.rc1",
+    description='Practice your flashcards using python',
     author='mike-fmh',
     author_email='mikemh@uri.edu',
     license='MIT',
     packages=find_packages(),
+    entry_points={'console_scripts': ['flashquiz = flashquiz.quizzer:main']},
     install_requires=[
         'pygame',
         'argparse'
